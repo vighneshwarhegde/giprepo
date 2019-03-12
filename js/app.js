34f8111b92315b8b89b9e084286c7335d678fde5
Harini Vijayakumar <harini.vijayakumar@sap.com>
2019-03-12 14:45:01 +0530

    added fields

--- js/app.js
+++ js/app.js
@@ -1,6 +1,18 @@
 var app = angular.module("gip",[]);
 app.controller('myCtrl',function($scope){
     $scope.versions=[2.1,2.2,2.3]
+    
 });
+app.filter('range', function() {
+    return function(input, total) {
+      total = parseInt(total);
+  
+      for (var i=0; i<total; i++) {
+        input.push(i);
+      }
+  
+      return input;
+    };
+  });
 
   
